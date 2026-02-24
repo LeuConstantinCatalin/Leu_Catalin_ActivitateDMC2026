1d0a1f1e9dd8f5da476639d7cddf0f8adcf732b9
Leu Catalin <catalin.leu31@gmail.com>
2026-02-24 11:11:22 +0200

    Lab2

---
+++ Lab01/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lab1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.btnClick)
+        val textView = findViewById<TextView>(R.id.titleText)
+
+        var cnt = 0
+
+        button.setOnClickListener {
+            cnt++
+
+            textView.text = getString(R.string.bravo_text, cnt)
+        }
+    }
+}
